1e6185b189e19f50b84799148252196aa0b1d2f6
grasmarcussanti129 <wemmelkiarie@hotmail.com>
2025-09-06 13:52:02 -0500

    feat: initial commit for build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script sets up and runs the CI pipeline
+
+echo "Setting up CI pipeline..."
+
+# Add pipeline setup commands here
+
+echo "Running CI pipeline..."
+
+# Add commands to trigger the builds here
